a845d8ad12f93e59fccfca647a8cccdb999fc4df
p4m4p <myarctic@163.com>
2020-03-15 10:50:59 +0800

    依赖、配置初始化

---
+++ src/main/java/org/javaboy/cacheredis/CacheRedisApplication.java
@@ -0,0 +1,13 @@
+package org.javaboy.cacheredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javaboy/cacheredis/CacheRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaboy.cacheredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
