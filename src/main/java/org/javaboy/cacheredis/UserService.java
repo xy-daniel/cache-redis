c1d2c6da6fe87a2a69440a0ccec31ac9a4a490ff
p4m4p <myarctic@163.com>
2020-03-16 22:17:34 +0800

    缓存配置使用@CacheConfig注解同意配置到类上

--- src/main/java/org/javaboy/cacheredis/UserService.java
+++ src/main/java/org/javaboy/cacheredis/UserService.java
@@ -1,5 +1,6 @@
 package org.javaboy.cacheredis;
 
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
  * @date 2020/3/15 10:53
  */
 @Service
+//缓存的配置除了在下面的每一个方法上面进行配置当然也可以同意在这个类上进行配置四种东西cacheName,KeyGenerator,cacheManager,cacheResolver
+@CacheConfig(cacheNames = "c1", keyGenerator = "myKeyGenerator")
 public class UserService {
 
     //方法开启缓存---->默认情况方法的参数就是缓存的key,方法的返回值就是缓存的value---->get "c1::1"/"c1::2"---->get "c1::SimpleKey [1,name]"/"c1::SimpleKey [1,name1]"(只有参数一样的时候才会调用缓存)
@@ -30,7 +33,7 @@ public class UserService {
     }
 
     //更新
-    @CachePut(cacheNames = "c1")
+     @CachePut(cacheNames = "c1")
     public User updateUserById(Integer id){
         System.out.println("updateUserById>>>>id:" + id);
         return new User(1, "name_update", "author_update");
